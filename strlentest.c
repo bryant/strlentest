226bdd7ec42e85737596719e7f538b33332846bd
bryant <bryant.wong@stonybrook.edu>
2013-02-11 23:36:49 +0100

    try de-aliasing. same shit.

--- strlentest.c
+++ strlentest.c
@@ -4,17 +4,19 @@
 
 typedef unsigned long long ull;
 
+void fn(const char* const str) {
+    for (unsigned int i=0; i<strlen(str); i++) {
+        printf("%c\n", str[i]);
+    }
+}
+
 int main(int argc, char** argv) {
     if (argc != 2) {
         printf("Usage: %s string\n", argv[0]);
         return -1;
     }
 
-    const char* k = argv[1];
-
-    for (unsigned int i=0; i<strlen(k); i++) {
-        printf("%c\n", k[i]);
-    }
+    fn((const char* const) argv[1]);
 
     return 0;
 }
